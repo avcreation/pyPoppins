4ff709f80999bfc512d23a25d4236108c067cc31
Alexandre Voiney <alexandre.voiney@arkena.com>
2014-10-17 20:58:32 +0200

    Add a docstring to the poppinsbag module

--- poppinsbag/__init__.py
+++ poppinsbag/__init__.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+""" Mary Poppins always has what you need in her bag.
+    This is a set of tools and helpers to deals with common things.
+    Give it a try !
+"""
 
 __version__ = "0.1.0"
