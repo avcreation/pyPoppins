45025ef73ee2765f57385fc97e155bda8f6192bc
Alexandre Voiney <alexandre.voiney@arkena.com>
2014-11-01 11:48:26 +0100

    Change the way Color is init
    Add a way to init color with string

--- poppinsbag/colors.py
+++ poppinsbag/colors.py
@@ -7,27 +7,29 @@ import hashlib
 
 class Color(object):
     """ Object to create, convert and manipulate color in python
-        >>> c = Color()
-        >>> c.set_rgb_from_string(b'network')
-        >>> c.get_rgb()
-        (210, 12, 100)
-        >>> c = Color()
-        >>> c.set_hex_from_string(b'network')
+        >>> c = Color(string=b'network')
         >>> c.get_rgb()
         (210, 12, 100)
         >>> c.get_hex()
         '#d20c64'
     """
-
-    def __init__(self, rgb=(0, 0, 0), hex_rgb="#000000"):
+    def __init__(self, rgb=(0, 0, 0), hex_rgb="#000000", string=None):
         """ Color object constuctor
             :param: rgb: The RGB int tuple of this color
             :type rgb: :py:class `tuple`
             :param: hex_rgb: The hexadecimale rgb of this color
             :type hex_rgb: :py:class `str`
+            :param: string: A string to convert into a color
+            :type string: :py:class `str`
         """
-        self._rgb = rgb
-        self._hex_rgb = hex_rgb
+        if not hex_rgb == "#000000":
+            self._hex_rgb = hex_rgb
+            self._rgb = self.convert_hex_to_rgb()
+        elif string:
+            self.set_hex_from_string(string)
+        else:
+            self._rgb = rgb
+            self._hex = self.convert_rgb_to_hex()
 
     def __repr__(self):
         return "<Color r:%s, g:%s, b:%s>" % self.get_rgb()
@@ -39,6 +41,36 @@ class Color(object):
     def get_rgb(self):
         """ Get the rgb representation of the color. """
         return self._rgb
+
+    def set_hex_from_rgb(self):
+        """ set the HEX color value """
+        self._hex_rgb = self.convert_rgb_to_hex()
+
+    def set_rgb_from_hex(self):
+        """ set the RGB from the HEX value """
+        self._rgb = self.convert_hex_to_rgb()
+
+    def convert_rgb_to_hex(self):
+        """ Convert RGB color to HEX color """
+        # unpack rgb
+        (red, green, blue) = self.get_rgb()
+        # Then we get the hex value, keep only the interesting part
+        # since hex(int) return 0x??.
+        # At the end, fill result with 0 to get a two characters string
+        hexred = hex(int(red))[2:].zfill(2)
+        # do this for the rest of color parts
+        hexgreen = hex(int(green))[2:].zfill(2)
+        hexblue = hex(int(blue))[2:].zfill(2)
+        # return the concatenation or hex_{r,g,b}, prefixed by `#`
+        return "#%s" % "".join((hexred, hexgreen, hexblue))
+
+    def convert_hex_to_rgb(self):
+        """ Convert HEX color to RGB color """
+        # split hex
+        _ = self.get_hex()[1:]
+        return (int(_[:2], 16),
+                int(_[2:4], 16),
+                int(_[4:], 16))
 
     def set_rgb_from_string(self, string):
         """ Set the rgb color representation of
@@ -63,21 +95,11 @@ class Color(object):
         self._rgb = (int_r, int_g, int_b)
 
     def set_hex_from_string(self, string):
-        """ Set the hex color representation of
+        """ Set the HEX color representation of
             a given string.
             :param: string: The string to colorize_string
             :type string: :py:class `bytes`
-       """
-        # Set the RGB of the object
+        """
+        # convert string to RGB then RGB to HEX and HEX TO HSL
         self.set_rgb_from_string(string)
-        # unpack rgb
-        (int_r, int_g, int_b) = self.get_rgb()
-        # Then we get the hex value, keep only the interesting part
-        # since hex(int) return 0x??.
-        # At the end, fill result with 0 to get a two characters string
-        hex_r = hex(int_r)[2:].zfill(2)
-        # do this for the rest of color parts
-        hex_g = hex(int_g)[2:].zfill(2)
-        hex_b = hex(int_b)[2:].zfill(2)
-        # return the concatenation or hex_{r,g,b}, prefixed by `#`
-        self._hex_rgb = "#%s" % "".join((hex_r, hex_g, hex_b))
+        self.set_hex_from_rgb()
